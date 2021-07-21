4c91e434c1ca288424aceac5e682b821bac3bda2
seomentor <87497356+seomentor@users.noreply.github.com>
2021-07-21 21:28:33 +0300

    Update domaintool.py

--- domaintool.py
+++ domaintool.py
@@ -3,19 +3,68 @@ import os.path
 import requests
 import json
 from datetime import datetime, timedelta
+from requests import Request, Session
 # Welcome Message
-print('Hello and Welcome to seoMentor Domain Purchase Automation Tool\nLet me check few things!')
+print('Hello and Welcome to seoMentor Domain Purchase Automation Tool\nLet me check few things....')
 
 def getwapi():
     if os.path.isfile('./Live DNS Project/whoisapikey.txt'):
         print('its look like its not your first time here. lets go!')
         pass
     else:
+        print('Its look like you are first time here, lets start gather some information about you')
         wapi = input(f'Enter your "WhoisXMLAPI.com" API Key: ')
         wapifile = open('./Live DNS Project/whoisapikey.txt', "w+")
         wapifile.write(wapi)
         wapifile.close()
 getwapi()
+
+def getlivedns():
+    if os.path.isfile('./Live DNS Project/livednsurl.txt'):
+        pass
+    else:
+        print('Now i need your Live DNS Details, its lil bit long but you do it one time.')
+        UserName = 'apidemo@livedns.co.il'
+        Password = 'demo'
+        RegistrantName = f'sapir%20zuberi'
+        RegistrantEmail = 'zuberi'
+        RegistrantAddress = 'avraham yafe 11'
+        RegistrantCity = 'holon'
+        RegistrantZipCode = '324234'
+        RegistrantCountry = 'IL'
+        RegistrantPhoneCountryCode = '972'
+        RegistrantPhoneCityCode = '03'
+        RegistrantPhoneNumber = '559282887'
+        AdminNicHandle = 'shay'
+        TechnicalNicHandle = 'adjkhas'
+        ZoneNicHandle = 'asdasd'
+        NS1 = 'asdasd'
+        NS2 = 'asdasd'
+        NS3 = 'asdasd'
+        lapiurl = 'https://domains.livedns.co.il/API/DomainsAPI.asmx/NewDomain?'
+        payload = {
+            'UserName': UserName,
+            'Password': Password,
+            'RegistrantName': RegistrantName,
+            'RegistrantEmail': RegistrantEmail,
+            'RegistrantAddress': RegistrantAddress,
+            'RegistrantCity': RegistrantCity,
+            'RegistrantZipCode': RegistrantZipCode,
+            'RegistrantCountry': RegistrantCountry,
+            'RegistrantPhoneCountryCode': RegistrantPhoneCountryCode,
+            'RegistrantPhoneCityCode': RegistrantPhoneCityCode,
+            'RegistrantPhoneNumber': RegistrantPhoneNumber,
+            'AdminNicHandle': AdminNicHandle,
+            'TechnicalNicHandle': TechnicalNicHandle,
+            'ZoneNicHandle': ZoneNicHandle,
+            'NS1': NS1,
+            'NS2': NS2,
+            'NS3': NS3}
+        ldfile = open('./Live DNS Project/livednsurl.txt', "w+")
+        writeapi = f'https://domains.livedns.co.il/API/DomainsAPI.asmx/NewDomain?UserName={UserName}&Password={Password}&DomainName=&RegistrationPeriod=&RegistrantName={RegistrantName}&RegistrantEmail={RegistrantEmail}&RegistrantAddress={RegistrantAddress}&RegistrantCity={RegistrantCity}&RegistrantZipCode={RegistrantZipCode}&RegistrantCountry={RegistrantCountry}&RegistrantPhoneCountryCode={RegistrantPhoneCountryCode}&RegistrantPhoneCityCode={RegistrantPhoneCityCode}&RegistrantPhoneNumber={RegistrantPhoneNumber}&AdminNicHandle={AdminNicHandle}&TechnicalNicHandle={TechnicalNicHandle}&ZoneNicHandle={ZoneNicHandle}&NS1={NS1}&NS2={NS2}&NS3={NS3}'
+        ldfile.write(str(writeapi))
+        ldfile.close()
+getlivedns()
 
 # ask which domain he want to buy
 domainBuy = str(input('Please Enter Domain Name: '))
@@ -40,7 +89,7 @@ questioncheck(qCheck)
 wapiurl = 'https://www.whoisxmlapi.com/whoisserver/WhoisService?'
 #api payload
 payload = {'apiKey': 'at_ibMNCbFfUGzZW41eRSXMKapbLUKEW', 'domainName': {domainBuy}, 'outputFormat': 'JSON'}
-whois = requests.post(wapiurl, params = payload)
+whois = requests.post(wapiurl, params=payload)
 data = json.loads(whois.content)
 current_date = data['WhoisRecord']['registryData']['expiresDate']
 date_obj = datetime.strptime(current_date, '%d-%m-%Y')
@@ -64,46 +113,3 @@ def clogin():
         print(r.headers)
     else:
         pass
-#Purchase Domain
-def dPurchase():
-    UserName = 'apidemo@livedns.co.il'
-    Password = 'demo'
-    DomainName = domainBuy
-    RegistrationPeriod = '1'
-    RegistrantName = f'sapir%20zuberi'
-    RegistrantEmail = 'zuberi'
-    RegistrantAddress = 'avraham yafe 11'
-    RegistrantCity = 'holon'
-    RegistrantZipCode = '324234'
-    RegistrantCountry = 'IL'
-    RegistrantPhoneCountryCode = '972'
-    RegistrantPhoneCityCode = '03'
-    RegistrantPhoneNumber = '559282887'
-    AdminNicHandle = 'shay'
-    TechnicalNicHandle = 'adjkhas'
-    ZoneNicHandle = 'asdasd'
-    NS1 = 'asdasd'
-    NS2 = 'asdasd'
-    NS3 = 'asdasd'
-    lapiurl = 'https://domains.livedns.co.il/API/DomainsAPI.asmx/NewDomain?'
-    payload = {
-        'UserName': UserName,
-        'Password': Password,
-        'DomainName': DomainName,
-        'RegistrationPeriod': RegistrationPeriod,
-        'RegistrantName': RegistrantName,
-        'RegistrantEmail': RegistrantEmail,
-        'RegistrantAddress': RegistrantAddress,
-        'RegistrantCity': RegistrantCity,
-        'RegistrantZipCode': RegistrantZipCode,
-        'RegistrantCountry': RegistrantCountry,
-        'RegistrantPhoneCountryCode': RegistrantPhoneCountryCode,
-        'RegistrantPhoneCityCode': RegistrantPhoneCityCode,
-        'RegistrantPhoneNumber': RegistrantPhoneNumber,
-        'AdminNicHandle': AdminNicHandle,
-        'TechnicalNicHandle': TechnicalNicHandle,
-        'ZoneNicHandle': ZoneNicHandle,
-        'NS1': NS1,
-        'NS2': NS2,
-        'NS3': NS3 }
-    requests.get(lapiurl, params=payload)
