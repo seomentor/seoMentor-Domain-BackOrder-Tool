13fc0b1c6b64f56812d736f1f63ee12144a96ac1
Shay Amos <87497356+seomentor@users.noreply.github.com>
2021-07-29 05:06:32 +0300

    Update domaintool.py

--- domaintool.py
+++ domaintool.py
@@ -1,19 +1,20 @@
 import requests
 import time
+import validators
 from datetime import datetime
 from datetime import timedelta
-# set a variable for setting.txt
-settingsFile = 'settings.txt'
-# open the file in read mode
-f = open(settingsFile, 'r')
 #ask which domain the user want to buy
 def askfordomain():
-    ask = input('Which Domain You want to Hunt?: ')
-    DomainName = ask
-    return DomainName
+    while True:
+        ask = input("Enter domain: ")
+        if validators.domain(ask):
+            return ask
+        else:
+            print("This is not a domain")
 # define a function that handle the setting and make setlist dict with all details
 def getSettingsFromFile():
-    # save settings value into fileContent variable
+    settingsFile = 'settings.txt'
+    f = open(settingsFile, 'r')
     fileContent = f.read()
     # define a dictionary
     setdict = {}
@@ -26,8 +27,8 @@ def getSettingsFromFile():
     # return the function the last value setdict and in fact getSettingsFromFile() function will return us dict list with our values
     return setdict
 # define function that prepare the last url for http post request
-def makelasturl():
-    lastdiclist = getSettingsFromFile()
+def makelasturl(validDomain, getset):
+    lastdiclist = getset
     UserName = lastdiclist['UserName']
     Password = lastdiclist['Password']
     RegistrantName = lastdiclist['RegistrantName']
@@ -46,14 +47,14 @@ def makelasturl():
     NS2 = lastdiclist['NS2']
     NS3 = lastdiclist['NS3']
     url = f'https://domains.livedns.co.il/API/DomainsAPI.asmx/NewDomain?'
-    lasturl = f'{url}UserName={UserName}&Password={Password}&DomainName={askfordomain()}RegistrantName={RegistrantName}&RegistrantEmail={RegistrantEmail}&RegistrantAddress={RegistrantAddress}&RegistrantCity={RegistrantCity}&RegistrantZipCode={RegistrantZipCode}&RegistrantCountry={RegistrantCountry}&RegistrantPhoneCountryCode={RegistrantPhoneCountryCode}&RegistrantPhoneCityCode={RegistrantPhoneCityCode}&RegisrantPhoneNumber={RegistrantPhoneNumber}&AdminNicHandle={AdminNicHandle}&TechnicalNicHandle={TechnicalNicHandle}&ZoneNicHandle={ZoneNicHandle}&NS1={NS1}&NS2={NS2}&NS3={NS3}'
+    lasturl = f'{url}UserName={UserName}&Password={Password}&DomainName={validDomain}RegistrantName={RegistrantName}&RegistrantEmail={RegistrantEmail}&RegistrantAddress={RegistrantAddress}&RegistrantCity={RegistrantCity}&RegistrantZipCode={RegistrantZipCode}&RegistrantCountry={RegistrantCountry}&RegistrantPhoneCountryCode={RegistrantPhoneCountryCode}&RegistrantPhoneCityCode={RegistrantPhoneCityCode}&RegisrantPhoneNumber={RegistrantPhoneNumber}&AdminNicHandle={AdminNicHandle}&TechnicalNicHandle={TechnicalNicHandle}&ZoneNicHandle={ZoneNicHandle}&NS1={NS1}&NS2={NS2}&NS3={NS3}'
     return lasturl
 # define a function that make a url for request and send post request to get expiry date and convert the xml result to json
-def getexpiresdate():
-    lastdiclist = getSettingsFromFile()
+def getexpiresdate(validDomain, getset):
+    lastdiclist = getset
     WhoApi = lastdiclist['wxakey']
     url = f'https://www.whoisxmlapi.com/whoisserver/WhoisService?'
-    apiurl = f'{url}apiKey={WhoApi}&domainName={askfordomain()}&outputFormat=JSON&ignoreRawTexts=1'
+    apiurl = f'{url}apiKey={WhoApi}&domainName={validDomain}&outputFormat=JSON&ignoreRawTexts=1'
     request = requests.get(apiurl)
     results = request.json()
     if 'expiresDate' in results['WhoisRecord']['registryData'].keys():
@@ -61,26 +62,27 @@ def getexpiresdate():
         return edate
     elif 'dataError' in results['WhoisRecord']['registryData'].keys():
         print('domain is available for purchase now! restart the program')
+        main()
 # Define function that calulcate the date and add more 90 days and 1 second for last date and return the total seconds until the date purchase
-def cpdate():
-    date = getexpiresdate()
+def cpdate(validDate):
+    date = validDate
     today = datetime.today()
     dt = datetime.strptime(date,'%d-%m-%Y')
     ddelay = timedelta(days=90, seconds=1)
     ldate = dt + ddelay
+    print('This Domain will be Available at: ' + str(ldate))
     cdate = ldate - today
     tsec = (cdate.total_seconds())
     return tsec
 # define the last function that buy the domain
-def domainbuy():
-    lurl = makelasturl()
+def domainbuy(lurl):
+    lurl = lurl
     request = requests.get(lurl)
     result = request.text
     return result
 # define main function that make everything complete
 def main():
-    if True:
-        print("""
+    print("""
                                       $$\      $$\                       $$\                         
                                       $$$\    $$$ |                      $$ |                        
          $$$$$$$\  $$$$$$\   $$$$$$\  $$$$\  $$$$ | $$$$$$\  $$$$$$$\  $$$$$$\    $$$$$$\   $$$$$$\  
@@ -99,22 +101,23 @@ def main():
                                                                              
          """)
     print('Hello and Welcome to seoMentor Domain Back Order System')
-    getSettingsFromFile()
-    makelasturl()
-    askfordomain()
-    getexpiresdate()
-    print('This Domain will be Available at: ' + getexpiresdate())
-    vask = input(f'Are you want to Schedule Purchase? write "yes" or "not"')
+    getset = getSettingsFromFile()
+    validDomain = askfordomain()
+    validDate = getexpiresdate(validDomain, getset)
+    datesec = cpdate(validDate)
+    lurl = makelasturl(validDomain, getset)
+    vask = input(f'Are you want to Schedule Purchase? write "yes" or "not": ')
     if 'yes' in vask:
-        d = cpdate()
+        print('Schedule a purchase, please dont close me.')
+        d = datesec
         time.sleep(d)
-        domainbuy()
-        if 'Success' in domainbuy():
+        domainbuy(lurl)
+        if 'Success' in domainbuy(lurl):
             print('Purchase Success')
-        elif 'Specified domain is unavailable for registration' in domainbuy():
+        elif 'Specified domain is unavailable for registration' in domainbuy(lurl):
             print('Purchase failed, sending more request in 3 seconds')
             time.sleep(3)
-            domainbuy()
+            domainbuy(lurl)
     else:
         exit()
 if __name__ == "__main__":
